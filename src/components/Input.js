a8beefe1bf22520b79748b8c5f0b441c8bcc779e
devmariano <developer.mariano@gmail.com>
2023-05-31 20:22:13 -0300

    upload inicial

---
+++ src/components/Input.js
@@ -0,0 +1,12 @@
+import "../styles/Input.css";
+import React from 'react';
+
+function Input () {
+    return ( 
+    <div className='Input'>
+    <input type="text" placeholder="Pesquise no Google ou escreva um url" />
+    </div>
+     );
+}
+
+export default Input;
