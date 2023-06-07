420fa7e40b0c54495055d3e84d68ac8fa2a4b330
devmariano <developer.mariano@gmail.com>
2023-06-06 22:02:36 -0300

    inserção do footer

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import googleLogo from './images/googlelogo.png';
 import Header from "./components/Header";
 import Input from "./components/Input";
 import Button from "./components/Button";
+import Footer from "./components/Footer";
 
 function App() {
-
-  
   return (
     <div className="App">
       <Header/>
       <img className="app-logo" src={googleLogo} alt="" />
-      <Input />
-      <Button />
+      <Input prop1="Pesquise no Google ou escreva um url" prop2={2023}/>
+      <Button prop1='Pesquisa Google' prop2={42} prop3/>
+      <Button prop1='Estou com sorte' prop2={12} prop3={false}/>
+      <Footer/>
     </div>
   );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,31 @@
 import "../styles/Button.css";
-
-
 import React from "react";
-let name1 = "Pesquisa Google";
-let name2 = "Estou com Sorte";
+import PropTypes from 'prop-types';
 
-function Button() {
+//exemplo de props 
+
+  /*function Button(props) {
+    if (props.prop){
+        return (<h1>"Hello"</h1>)
+    }
+    //abaixo é igual ao else
     return (
-      <div>
-        <button className="Btn">{name1}</button>
-        <button className="Btn">{name2}</button>
-      </div>
+      <React.Fragment>
+        <button className="Btn">{props.prop1}</button>
+      </React.Fragment>
     );
-  }
+  }*/
+  function Button(props) {
+    return  (
+    <React.Fragment>
+      <button className="Btn" type="submit">{props.prop1}</button>
+    </React.Fragment>
+    );
+    }
+    Button.propTypes = {
+      prop1: PropTypes.string.isRequired,
+      prop2: PropTypes.number.isRequired,
+      prop3: PropTypes.bool,
+    }
   
   export default Button;
